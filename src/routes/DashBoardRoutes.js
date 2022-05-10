bc329a9feafc71ead5c15f95ff78f083eef36554
Kevin <j1120531@gmail.com>
2022-05-09 21:29:31 -0500

    solucion a error con isHost

--- src/routes/DashBoardRoutes.js
+++ src/routes/DashBoardRoutes.js
@@ -1,16 +1,13 @@
+import { useSelector } from 'react-redux'
 import { Route, Routes, Navigate } from 'react-router-dom'
-import AddEstadia from '../components/AddEstadia'
 import Home from '../components/Home'
 
 const DashBoardRoutes = ({ isHost }) => {
-
-    console.log(isHost)
-
+    
     return (
         <div>
             <Routes>
                 <Route path="/" element={<Home />} />
-                {isHost ? <Route path="/add" element={<AddEstadia />} /> : null}
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </div>
